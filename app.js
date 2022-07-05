48b6ea8eac90839d8ac30e0b0a896231fd8eee01
Luke <rugulous1999@hotmail.com>
2022-07-05 16:57:48 +0100

    Generate and store rankings

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const DCI = require('./lib/DCI');
 
-const { Sequelize } = require('sequelize');
+const { Sequelize, QueryTypes } = require('sequelize');
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: './db.sqlite'
@@ -59,7 +59,59 @@ async function populateData(db) {
 }
 
 async function updateRankings(db) {
-    //empty - for now
+    const date = new Date('2022-07-02');
+
+    let exists = await db.Ranking.findOne({
+        where: {
+            RankingDate: date
+        }
+    });
+
+    if(exists != null){
+        console.log("Ranking already created!");
+        return;
+    }
+
+    const ranks = await sequelize.query(`
+    SELECT c.Name, p.Division, c.ExternalID, p.Score, s.Date 
+    FROM Corps c
+    INNER JOIN Performances p ON p.id = (
+	    SELECT p1.id
+	    FROM Performances p1
+	    INNER JOIN Shows s ON s.ExternalID = p1.ShowID
+	    WHERE p1.CorpsID = c.ExternalID
+	    AND p1.Score > 0
+	    AND strftime('%Y', s.Date) = $1
+	    ORDER BY s.Date DESC
+	    LIMIT 1
+    )
+    INNER JOIN Shows s ON s.ExternalID = p.ShowID
+
+    ORDER BY p.Score DESC
+    `, {
+        bind: [String(date.getFullYear())],
+        type: QueryTypes.SELECT
+    });
+
+    let divCount = {};
+    for (let i = 0; i < ranks.length; i++) {
+        if(divCount.hasOwnProperty(ranks[i].Division)){
+            divCount[ranks[i].Division]++;
+        } else {
+            divCount[ranks[i].Division] = 1;
+        }
+
+        console.log(`${divCount[ranks[i].Division]}: ${ranks[i].Name}`);
+
+        await db.Ranking.create({
+            Division: ranks[i].Division,
+            CorpsID: ranks[i].ExternalID,
+            Score: ranks[i].Score,
+            Rank: divCount[ranks[i].Division],
+            RankingDate: date,
+            LastShowDate: ranks[i].Date
+        });
+    }
 }
 
 async function doPost(db) {
@@ -69,7 +121,7 @@ async function doPost(db) {
 async function main() {
     const db = await require('./lib/models')(sequelize);
 
-    await populateData(db);
+    //await populateData(db);
     await updateRankings(db);
     await doPost(db);
 
