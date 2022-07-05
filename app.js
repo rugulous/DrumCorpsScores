2885dc9576d5fe97b570b0175e77ab6bc2319ca1
Luke <rugulous1999@hotmail.com>
2022-07-05 13:00:58 +0100

    Get events

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const DCI = require('./lib/DCI');
 async function main(){
     const corps = await DCI.getCorps();
     console.log(corps);
+
+    console.log("");
+
+    const events = await DCI.getEvents();
+    console.log(events);
 }
 
 main();
