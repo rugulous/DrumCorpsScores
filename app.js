29e959f7132df3bd12032a2615d4e156a6082049
Luke <rugulous1999@hotmail.com>
2022-07-05 15:20:00 +0100

    Save corps to DB

--- app.js
+++ app.js
@@ -8,22 +8,27 @@ const sequelize = new Sequelize({
     storage: './db.sqlite'
 });
 
-const db = require('./lib/models')(sequelize);
-
-async function main(){
+async function main() {
+    const db = await require('./lib/models')(sequelize);
+    
+    //we may not have any bands in our table yet
+    //let's double-check these at the start!
     const corps = await DCI.getCorps();
-    //console.log(corps);
-
-    //console.log("");
-
-    const events = await DCI.getEvents();
-    //console.log(events);
+    corps.forEach(async (c) => {
+        await db.Corps.upsert({
+            Name: c.name,
+            Slug: c.slug,
+            ExternalID: c.id
+        });
+    })
 
     const dEvents = await DCI.getEventsForDate(new Date());
     console.log("Today's shows are:");
     dEvents.forEach(e => {
         console.log(`- ${e.eventName}, at ${e.venue.name}`);
-    })
+    });
+
+    await db.sequelize.close();
 }
 
 main();
