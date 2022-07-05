f966694ea6c616a725795a3f6fb204fd4c338b00
Luke <rugulous1999@hotmail.com>
2022-07-05 16:05:21 +0100

    Save performances/scores in DB

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ const sequelize = new Sequelize({
     storage: './db.sqlite'
 });
 
-async function main() {
-    const db = await require('./lib/models')(sequelize);
-    
+async function populateData(db) {
     //we may not have any bands in our table yet
     //let's double-check these at the start!
     const corps = await DCI.getCorps();
@@ -26,7 +24,7 @@ async function main() {
 
     const events = await DCI.getEventsForDate(date);
     console.log("Today's shows are:");
-    for(let i = 0; i < events.length; i++){
+    for (let i = 0; i < events.length; i++) {
         const e = events[i];
         console.log(e.name);
 
@@ -36,7 +34,44 @@ async function main() {
             Slug: e.slug,
             ExternalID: e.id
         });
+
+        const show = await DCI.getShowDetails(e.slug);
+        for (let j = 0; j < show.length; j++) {
+            const p = show[j];
+
+            const exists = await db.Performance.findOne({
+                where: {
+                    ShowID: e.id,
+                    CorpsID: p.orgGroupIdentifier
+                }
+            });
+
+            if (exists === null) {
+                await db.Performance.create({
+                    ShowID: e.id,
+                    CorpsID: p.orgGroupIdentifier,
+                    Score: p.totalScore,
+                    Division: p.divisionName
+                });
+            }
+        }
     }
+}
+
+async function updateRankings(db) {
+    //empty - for now
+}
+
+async function doPost(db) {
+    //empty - for now
+}
+
+async function main() {
+    const db = await require('./lib/models')(sequelize);
+
+    await populateData(db);
+    await updateRankings(db);
+    await doPost(db);
 
     await db.sequelize.close();
 }
