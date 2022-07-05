9207fdd473cbef5d24f0072061bc8dcd1634e547
Luke <rugulous1999@hotmail.com>
2022-07-05 15:01:10 +0100

    Sequelize bootstrapping

--- app.js
+++ app.js
@@ -2,14 +2,22 @@
 
 const DCI = require('./lib/DCI');
 
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db.sqlite'
+});
+
+const db = require('./lib/models')(sequelize);
+
 async function main(){
     const corps = await DCI.getCorps();
-    console.log(corps);
+    //console.log(corps);
 
-    console.log("");
+    //console.log("");
 
     const events = await DCI.getEvents();
-    console.log(events);
+    //console.log(events);
 
     const dEvents = await DCI.getEventsForDate(new Date());
     console.log("Today's shows are:");
